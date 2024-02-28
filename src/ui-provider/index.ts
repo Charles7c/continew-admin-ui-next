f34e4e6e0e1703d25f322d7f069b3db7ed236006
Charles7c <charles7c@126.com>
2024-02-28 21:14:27 +0800

    chore: 新增 Element Plus 自定义主题配置

--- src/ui-provider/index.ts
+++ src/ui-provider/index.ts
@@ -1,7 +1,6 @@
 import type { App } from 'vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
+import '@/assets/styles/element-plus.scss'
 
 function install(app: App) {
   app.use(ElementPlus)
